4f1fea46f3de121c00013f030bd1b51193af955b
danieltsang <danieltsang@meta.com>
2023-03-13 17:25:58 -0400

    Update config

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
-const AUTH_TOKEN = ''; // eslint-disable-line
+const AUTH_TOKEN = '';
 
 const API_KEY = '';
 
-const VENUE_ID = '';
+const VENUE_ID = '34341'; // 42534 (dcp) 3065 (mace)
 
-const DAY = '2020-10-10';
+const DAY = '2023-03-18';
 
 const TIMES = [
+  '17:00:00',
+  '17:15:00',
+  '17:30:00',
+  '17:45:00',
   '18:00:00',
   '18:15:00',
   '18:30:00',
   '18:45:00',
   '19:00:00',
-  '19:15:00',
-  '19:30:00',
-  '19:45:00',
-  '20:00:00',
 ];
 
-const PARTY_SIZE = 4;
+const PARTY_SIZE = 2;
 
-const DINING_TYPE = 'Outdoor Dining'; // Don Angie
+const DINING_TYPE = ''; // Optional e.g Outdoor Dining for Don Angie
 
 module.exports = {
   AUTH_TOKEN,
